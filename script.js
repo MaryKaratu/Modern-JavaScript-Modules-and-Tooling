2faebdd19c5e5061c232ae395fc8e9ad7d247f78
MaryKaratu <Karatumary85@gmail.com>
2025-01-26 17:00:36 +0100

    CommonJS

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // //Importing module
 // // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 
+import { cart } from "./shoppingCart"
+
 // import shoppingCart from './shoppingCart';
 
 // // console.log('Importing module');
@@ -52,33 +54,44 @@
 //////////////
 //Module Pattern
 
-const ShoppingCart2 = (function () {
-  const cart = [];
-  const shippingCost = 10;
-  const totalPrice = 237;
-  const totalQuantity = 23;
+// const ShoppingCart2 = (function () {
+//   const cart = [];
+//   const shippingCost = 10;
+//   const totalPrice = 237;
+//   const totalQuantity = 23;
 
-  const addToCart = function (product, quantity) {
-    cart.push({ product, quantity });
-    console.log(
-      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
-    );
-  };
+//   const addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(
+//       `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+//     );
+//   };
 
-  const orderStock = function (product, quantity) {
-    cart.push({ product, quantity });
-    console.log(`${quantity} ${product} ordered from supplier`);
-  };
+//   const orderStock = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(`${quantity} ${product} ordered from supplier`);
+//   };
 
-  return {
-    addToCart,
-    cart,
-    totalPrice,
-    totalQuantity,
-  };
-})();
+//   return {
+//     addToCart,
+//     cart,
+//     totalPrice,
+//     totalQuantity,
+//   };
+// })();
 
-ShoppingCart2.addToCart('apple', 4);
-ShoppingCart2.addToCart('pizza', 2);
-console.log(ShoppingCart2);
-console.log(ShoppingCart2.shippingCost);
+// ShoppingCart2.addToCart('apple', 4);
+// ShoppingCart2.addToCart('pizza', 2);
+// console.log(ShoppingCart2);
+// console.log(ShoppingCart2.shippingCost);
+
+//////////////////////////////
+//Common Modules
+//Export
+export.addToCart = function(product, quantity){
+  cart.push({product, quantity});
+  console.log(`${quantity} ${product} added to cart (sipping cost is ${shippingCost})`
+  );
+}
+
+//Import
